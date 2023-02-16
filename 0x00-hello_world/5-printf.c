6f8c31ec33af6ca5a52698d77dd3b15e8e6af038
bosro <misterbosro@gmail.com>
2023-02-16 11:11:53 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  * main - This is the main/entry function
+  * printf - This prints to the standard output
+  *
+  * Return: This returns the value 0 when successful
+  */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
