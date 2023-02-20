765da56eb9c3ac9fae335129e38adedc9226b5c7
bosro <misterbosro@gmail.com>
2023-02-20 11:14:04 -0800

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+  * main - This is the main/entry function
+  * Return: This returns 0 after it the code
+  *
+  * runs successfully
+  */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
