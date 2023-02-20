62721da509381283560c64d75b645f41c4c77592
bosro <misterbosro@gmail.com>
2023-02-20 13:01:35 -0800

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+  * main - This is the main/entry function
+  * Return: This returns 0 after it the code
+  *
+  * runs successfully
+  */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}	
+	}
+	putchar('\n');
+	return (0);
+}
