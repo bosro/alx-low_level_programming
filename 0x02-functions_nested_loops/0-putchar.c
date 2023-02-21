5e98f5f66bfcb0241dd2f1d69ac7b85ded7b66b3
bosro <misterbosro@gmail.com>
2023-02-21 13:02:43 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+  *main - Main function contains all the code
+  *_putchar: function ptints the characters
+  *Return: function returns zero when
+  *program executes successfully
+  */
+
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
