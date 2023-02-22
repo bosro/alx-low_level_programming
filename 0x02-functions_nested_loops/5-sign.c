22537d22ccfdeade0af419037af949ed79ea98fa
bosro <misterbosro@gmail.com>
2023-02-22 13:01:00 -0800

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+
+/**
+ * print_sign - checks and print sign
+ *
+ * @n: function parameter tat checks the value
+ * Return: return 1 if + and 0 if equals to 0
+ * otherwise 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		_putchar(',');
+		_putchar(' ');
+		return (-1);
+	}
+}
