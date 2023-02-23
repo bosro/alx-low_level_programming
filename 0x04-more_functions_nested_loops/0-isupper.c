14a2b72e34ed1860bf15442c397b62b45e598a9e
bosro <misterbosro@gmail.com>
2023-02-23 10:51:05 -0800

    isuper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -10,12 +10,8 @@
 
 int _isupper(int c)
 {
-	for (c = 65; c <= 122; c++)
-	{
-		if (c > 90)
-			return (0);
-		else
-			return (1);
-	}
-	return (c);
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
 }
