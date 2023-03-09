57db4d4c842c7b3b32080ea7f0cf54151b8ee882
bosro <misterbosro@gmail.com>
2023-03-09 07:13:02 -0800

    Task

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - main function that implements max_PrimeNum
+ *
+ * Return: returns 0
+ */
+
+int main(long maxnum)
+{
+	long n;
+	n = 612852475143;
+	printf("%li\n", max_PrimeNum(n));
+	return (maxnum);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -12,5 +12,6 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+int max_PrimeNum(int n);
 
 #endif
